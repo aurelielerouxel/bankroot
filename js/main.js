49c4904e04c6f19c00af023296e12f6db60bdc24
Aurelie Lerouxel <malkiniou@gmail.com>
2020-08-28 14:10:32 +0200

    add layer of security rules

---
+++ js/main.js
@@ -0,0 +1,58 @@
+// Création du layer
+let main = document.getElementsByTagName("main")[0];
+let background = document.getElementById("background");
+let layer = document.getElementById("layer");
+
+layer.setAttribute("background", "id", "layer");
+main.appendChild(layer);
+
+let buttonWarningLayer = document.createElement("button");
+main.appendChild(buttonWarningLayer);
+
+// get layer's text with AJAX request
+let httpRequest = new XMLHttpRequest();
+
+httpRequest.onreadystatechange = function() {
+    if (httpRequest.readyState === XMLHttpRequest.DONE){
+
+        if(httpRequest.status === 200){
+
+            layer.innerText = httpRequest.responseText;
+        }
+    }
+}
+httpRequest.open("GET", "warning.txt", true);
+httpRequest.send();
+
+// button function
+buttonWarningLayer.addEventListener("click", function(){
+    layer.style.display = "none";
+    buttonWarningLayer.style.display = "none";
+});
+
+// style of background layer
+// background.style.backgroundColor = "black";
+// background.style.width = "100%";
+// background.style.height = "100%";
+// background.style.position = "absolute";
+
+// style of layer
+layer.style.width = "80%";
+layer.style.height = "auto";
+layer.style.position = "absolute";
+layer.style.textAlign = "center";
+layer.style.top = "10%"
+layer.style.opacity = "90%";
+layer.style.fontSize = "2em";
+layer.style.backgroundColor = "red";
+layer.style.color = "white";
+
+// style of button 
+buttonWarningLayer.innerText = "J'accepte les conditions d'utilisation.";
+buttonWarningLayer.style.padding = "1em";
+buttonWarningLayer.style.borderRadius = "25px";
+buttonWarningLayer.style.position = "relative";
+buttonWarningLayer.style.top= "90%";
+buttonWarningLayer.style.left = "25%";
+buttonWarningLayer.style.backgroundColor = "black";
+buttonWarningLayer.style.color = "white";
